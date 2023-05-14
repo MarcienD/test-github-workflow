0595394dc9af4bcb81b4c376c78b08321b483ce8
MarcienD <114868544+MarcienD@users.noreply.github.com>
2023-05-14 21:52:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 def index():
     '''
     Wyświetlamy powitanie użytkownika
-    """
+    '''
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 if __name__ == "__main__":
     app.run(debug=True)
